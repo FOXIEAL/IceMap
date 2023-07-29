721ed04966fbe148b50250838359bb33ca05390a
waigoma <waichi0302@gmail.com>
2023-07-30 03:23:59 +0900

    Firestore を扱うための behaviour と manager の作成

---
+++ Assets/IceMap/Firestore/FirestoreBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using Firebase;
+using Firebase.Firestore;
+using UnityEngine;
+
+namespace IceMap.Firestore
+{
+    public class FirestoreBehaviour : MonoBehaviour
+    {
+        public FirestoreManager Manager { get; private set; }
+        
+        private void Start()
+        {
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(_ => {
+                var app = FirebaseApp.DefaultInstance;
+                var db = FirebaseFirestore.DefaultInstance;
+                Manager = new FirestoreManager(db);
+                Debug.Log("Firebase initialized successfully!");
+            });
+            
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/IceMap/Firestore/FirestoreManager.cs
@@ -0,0 +1,14 @@
+﻿using Firebase.Firestore;
+
+namespace IceMap.Firestore
+{
+    public class FirestoreManager
+    {
+        private FirebaseFirestore _db;
+        
+        public FirestoreManager(FirebaseFirestore db)
+        {
+            _db = db;
+        }
+    }
+}
